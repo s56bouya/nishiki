e6ce69a962517b6ab04460ca6fb04da305d2c746
s56bouya <s56bouya@gmail.com>
2017-10-04 18:19:52 +0900

    Add : Input type=radio style Change : Archive category block Change : Script and style handle name

--- library/script.php
+++ library/script.php
@@ -5,13 +5,13 @@ if ( ! function_exists('nishiki_read_scripts') ) :
 		// Add Script
 		wp_enqueue_script('jquery');
 		if ( is_single() ) wp_enqueue_script( "comment-reply" );
-		wp_register_script( 'nishiki-main-js', get_template_directory_uri() . '/js/main.js', array('jquery'), null, true );
-		wp_enqueue_script( 'nishiki-main-js' );
+		wp_register_script( 'nishiki-main-script', get_template_directory_uri() . '/js/main.js', array('jquery'), null, true );
+		wp_enqueue_script( 'nishiki-main-script' );
 
 		// Add Style
-		wp_register_style( 'nishiki-main-css', get_template_directory_uri() . '/main.css', array(), null, false );
-		wp_enqueue_style( 'nishiki-main-css' );
-		wp_add_inline_style( 'nishiki-main-css', nishiki_customizer_css() );
+		wp_register_style( 'nishiki-main-style', get_template_directory_uri() . '/main.css', array(), null, false );
+		wp_enqueue_style( 'nishiki-main-style' );
+		wp_add_inline_style( 'nishiki-main-style', nishiki_customizer_css() );
 
 	}
 	add_action( 'wp_enqueue_scripts', 'nishiki_read_scripts' );
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Nishiki ===
 Requires at least: WordPress 4.5
 Tested up to: WordPress 4.8.2
-Version: 0.1.7
+Version: 0.1.8
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Tags: one-column, two-columns, right-sidebar, left-sidebar, flexible-header, accessibility-ready, custom-colors, custom-header, custom-menu, custom-logo, featured-images, footer-widgets, post-formats, sticky-post, theme-options, threaded-comments, translation-ready, blog, entertainment
