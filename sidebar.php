760eb28b90e4ff0b0268ffe87db24f843601a39b
s56bouya <s56bouya@gmail.com>
2018-07-14 11:31:28 +0900

    Change : Several minor changes

--- sidebar.php
+++ sidebar.php
@@ -3,6 +3,6 @@ if ( ! is_active_sidebar( 'sidebar_main' ) ) {
 	return;
 }
 ?>
-<aside role="complementary">
+<aside class="sidebar" role="complementary">
 	<?php dynamic_sidebar( 'sidebar_main' ); ?>
 </aside>
