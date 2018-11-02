f5110984f3b8014f025b6420b9c9a81b58e9d750
s56bouya <s56bouya@gmail.com>
2018-11-02 20:43:37 +0900

    Add : Global nav do_action(nishiki_before_site_nav_inner_search_button, nishiki_after_site_nav_inner_search_button, nishiki_before_site_nav_inner_menu_collaple_button, nishiki_after_site_nav_inner_menu_collapse_button, nishiki_before_site_nav_inner_menu_button, nishiki_after_site_nav_inner_menu_button) Change : Read global nav in global-nav.php

--- parts/header/content.php
+++ parts/header/content.php
@@ -18,44 +18,7 @@
 					</a>
 			</div>
 	  <?php } ?>
-
-	  <?php do_action( 'nishiki_before_site_nav' ); ?>
-		<nav class="global-nav" role="navigation">
-		<?php do_action( 'nishiki_before_site_nav_inner' ); ?>
-		<?php if( has_nav_menu( 'global' ) && get_theme_mod( 'setting_header_menu_collapse', false ) ){ ?>
-					<div class="menu-collapse panel" id="menu-collapse">
-			  <?php
-			  $nav_args = array(
-				  'theme_location' => 'global',
-				  'container_class' => '',
-				  'container' => '',
-				  'menu_id' => '',
-				  'items_wrap' => '<ul>%3$s</ul>',
-			  );
-			  wp_nav_menu($nav_args);
-			  ?>
-						<button id="close-panel-button" class="close"><i class="icomoon icon-close"></i></button>
-					</div>
-		<?php } ?>
-		<?php if( get_theme_mod( 'setting_header_search_button', true ) ){ ?>
-					<button id="search-button" class="icon">
-						<i class="icomoon icon-search"></i>
-					</button>
-		<?php } ?>
-		<?php if ( has_nav_menu( 'global' ) ) { ?>
-			<?php if( get_theme_mod( 'setting_header_menu_collapse', false ) ) { ?>
-						<button id="menu-collapse-button" class="icon" aria-controls="top-menu">
-							<i class="icomoon icon-menu2"></i>
-						</button>
-			<?php } else { ?>
-						<button id="menu-button" class="icon" aria-controls="top-menu">
-							<i class="icomoon icon-menu2"></i>
-						</button>
-			<?php } ?>
-		<?php } ?>
-		<?php do_action( 'nishiki_after_site_nav_inner' ); ?>
-		</nav>
-	  <?php do_action( 'nishiki_after_site_nav' ); ?>
+		<?php get_template_part( 'parts/header/global-nav' ); ?>
 	</div>
 </div>
 <?php do_action( 'nishiki_after_site_header' ); ?>
